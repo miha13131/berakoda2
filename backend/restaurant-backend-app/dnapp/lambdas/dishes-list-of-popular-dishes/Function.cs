2e820f0958a13a237ed5109ac4dd2a63edbfa0ac
miha13131 <87076262+miha13131@users.noreply.github.com>
2026-03-09 17:29:02 +0100

    Add files via upload

---
+++ backend/restaurant-backend-app/dnapp/lambdas/dishes-list-of-popular-dishes/Function.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using shared;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace SimpleLambdaFunction;
+
+public class Function
+{
+    private readonly IAmazonDynamoDB _client;
+
+    public Function()
+    {
+        _client = new AmazonDynamoDBClient();
+    } 
+    
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        var response = await _client.ScanAsync(new ScanRequest { TableName = "OrderItems" });
+        
+        var mapped = response.Items
+            .Select(item => new
+            {
+                DishId = item["dish_id"].N,
+                Quantity = int.Parse(item["quantity"].N)
+            })
+            .ToList();
+
+        var grouped = mapped
+            .GroupBy(x => x.DishId)
+            .ToList();
+
+        var aggregated = grouped
+            .Select(group => new
+            {
+                DishId = group.Key,
+                TotalOrdered = group.Sum(x => x.Quantity)
+            })
+            .ToList();
+
+        var result = aggregated
+            .OrderByDescending(x => x.TotalOrdered)
+            .Take(6)
+            .ToList();
+        
+        var keys = result.Select(x => new Dictionary<string, AttributeValue>
+        {
+            { "dish_id", new AttributeValue { N = x.DishId } }
+        }).ToList();
+
+        var batchRequest = new BatchGetItemRequest
+        {
+            RequestItems = new Dictionary<string, KeysAndAttributes> {{ "Dishes", new KeysAndAttributes { Keys = keys }}}
+        };
+
+        var dishesResponse = await _client.BatchGetItemAsync(batchRequest);
+        
+        var dishes = dishesResponse.Responses["Dishes"];
+
+        var orderItems = dishes.Select(d => new DishDto
+        {
+            dishId = d["dish_id"].N,
+            dishName = d["dish_name"].S,
+            dishPrice = (d["dish_price"].S)
+        }).ToList();
+        
+        return ResponseCreator.CreateResponse(200, "Successful return of the list of locations!", new { popularDishes = orderItems });
+    }
+}
