dbb04d1e5fa8ebc1ef7fb128604658cb635903b8
miha13131 <87076262+miha13131@users.noreply.github.com>
2026-03-11 21:41:52 +0100

    Delete backend directory

--- backend/restaurant-backend-app/dnapp/lambdas/locations-full-list-of-restaurants/BriefLocationDto.cs
+++
@@ -1,7 +0,0 @@
-namespace SimpleLambdaFunction;
-
-public class BriefLocationDto
-{
-    public string id { get; set; } 
-    public string value { get; set; }
-}
--- backend/restaurant-backend-app/dnapp/lambdas/locations-full-list-of-restaurants/LocationDto.cs
+++
@@ -1,8 +0,0 @@
-namespace SimpleLambdaFunction;
-
-public class LocationDto
-{
-    public string id { get; set; } 
-    public string address { get; set; }
-    public string totalCapacity { get; set; }
-}
