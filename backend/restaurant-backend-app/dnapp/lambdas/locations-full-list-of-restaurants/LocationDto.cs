0809e1e32527e9ce151e525cb16c93ba12903330
miha13131 <87076262+miha13131@users.noreply.github.com>
2026-03-11 21:43:05 +0100

    Add files via upload

---
+++ backend/restaurant-backend-app/dnapp/lambdas/locations-full-list-of-restaurants/LocationDto.cs
@@ -0,0 +1,8 @@
+namespace SimpleLambdaFunction;
+
+public class LocationDto
+{
+    public string id { get; set; } 
+    public string address { get; set; }
+    public string totalCapacity { get; set; }
+}
