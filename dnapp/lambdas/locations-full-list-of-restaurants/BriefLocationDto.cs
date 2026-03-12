4c32356031c34ad7b7f29e43c4adc64714cc2061
miha13131 <87076262+miha13131@users.noreply.github.com>
2026-03-12 11:31:07 +0100

    Add files via upload

---
+++ dnapp/lambdas/locations-full-list-of-restaurants/BriefLocationDto.cs
@@ -0,0 +1,7 @@
+namespace SimpleLambdaFunction;
+
+public class BriefLocationDto
+{
+    public string id { get; set; } 
+    public string value { get; set; }
+}
---
+++ dnapp/lambdas/locations-full-list-of-restaurants/LocationDto.cs
@@ -0,0 +1,12 @@
+namespace SimpleLambdaFunction;
+
+public class LocationDto
+{
+    public string id { get; set; } 
+    public string address { get; set; }
+    public string totalCapacity { get; set; }
+    public string averageOccupancy { get; set; }
+    public string imageUrl { get; set; }
+    public string description { get; set; }
+    public string rating { get; set; }
+}
